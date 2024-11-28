ac16ece3f1ba95d2d907328b802208ad9e340250
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2024-11-28 02:04:47 -0600

    attempt fix

--- scripts/checkout.and.pull.develop.sh
+++ scripts/checkout.and.pull.develop.sh
@@ -9,20 +9,12 @@ PARENT=$(pwd)
 
 func_commands() {
     # git reset --hard
-    git checkout develop
+    git checkout main
     git pull
 }
 
-DIRECTORIES="applications common services"
-
-for d in $DIRECTORIES; do
-    echo "processing ${d}"
-    for a in ${PARENT}/${d}/*; do
-        echo "checking ${a}"
-        if [ -d "$a" ]; then
-            echo "processing ${a}"
-            cd ${a}
-            func_commands &
-        fi
-    done
+for a in $(find . -type d -name ".git"); do
+    echo "checking ${a}"
+    cd ${a}/..
+    func_commands &
 done
